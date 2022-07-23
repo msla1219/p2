c3ac29a76866fbd2c11bd05f686c88e2db6530d5
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-07-23 10:36:51 +0900

    Add files via upload

---
+++ order_book.py
@@ -0,0 +1,107 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from sqlalchemy import select
+from sqlalchemy.sql import text
+from sqlalchemy import Table, MetaData
+from sqlalchemy.sql import select, and_    
+
+from models import Base, Order
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Helper
+def print_it(title, results):
+    """Just a helper function to display database results"""
+    print("\n" + title)
+
+    for rec in results:
+        print(rec)
+        break       # just for one.
+
+
+def process_order(order):
+  #Your code here
+
+  #1. Insert new order
+  order_obj = Order(  sender_pk=order['sender_pk'],
+                      receiver_pk=order['receiver_pk'], 
+                      buy_currency=order['buy_currency'], 
+                      sell_currency=order['sell_currency'], 
+                      buy_amount=order['buy_amount'], 
+                      sell_amount=order['sell_amount'] )
+  session.add(order_obj)
+  session.commit()
+
+  # check up if it works well and get the order id
+  results = session.execute("select distinct id from orders where " + 
+                            " sender_pk = '" + str(order['sender_pk']) + "'" +
+                            " and receiver_pk = '" + str(order['receiver_pk']) + "'")
+
+  order_id = results.first()['id']
+
+  #2. Matching order
+  results = session.execute("select count(id) " + 
+                            "from orders where filled is null " + 
+                            " and (buy_amount/sell_amount) <= " + str(order['sell_amount']/order['buy_amount']) + 
+                            " and sell_currency = '" + order['buy_currency'] + "'" +
+                            " and buy_currency = '" + order['sell_currency'] + "'")
+  
+  if results.first()[0] == 0:
+    print("::::no matching order::::")
+    return
+  
+  results = session.execute("select distinct id, sender_pk, receiver_pk, buy_currency, sell_currency, buy_amount, sell_amount " + 
+                            "from orders where filled is null " + 
+                            " and (buy_amount/sell_amount) <= " + str(order['sell_amount']/order['buy_amount']) + 
+                            " and sell_currency = '" + order['buy_currency'] + "'" +
+                            " and buy_currency = '" + order['sell_currency'] + "'")
+
+  for row in results:
+    m_order_id = row['id']
+    m_sender_pk = row['sender_pk']
+    m_receiver_pk = row['receiver_pk'] 
+    m_buy_currency = row['buy_currency'] 
+    m_sell_currency = row['sell_currency'] 
+    m_buy_amount = row['buy_amount']
+    m_sell_amount = row['sell_amount']
+    print(" matched at ID: ", m_order_id)
+
+    if order['buy_amount'] < 0 or order['sell_amount'] < 0:
+        print("::: negative amount :::")
+
+    break
+
+  # update both the matching orders 
+  stmt = text("UPDATE orders SET counterparty_id=:id, filled=:curr_date WHERE id=:the_id")
+  stmt = stmt.bindparams(the_id=order_id, id=m_order_id, curr_date=datetime.now())
+  session.execute(stmt)  # where session has already been defined
+
+  stmt = text("UPDATE orders SET counterparty_id=:id, filled=:curr_date WHERE id=:the_id")
+  stmt = stmt.bindparams(the_id=m_order_id, id=order_id, curr_date=datetime.now())
+  session.execute(stmt)  # where session has already been defined
+  
+  #3. Create derived order
+  if order['buy_amount'] > m_sell_amount:
+    order_obj = Order(  sender_pk=order['sender_pk'],
+                        receiver_pk=order['receiver_pk'], 
+                        buy_currency=order['buy_currency'], 
+                        sell_currency=order['sell_currency'], 
+                        buy_amount=order['buy_amount'] - m_sell_amount, 
+                        sell_amount=order['sell_amount'] - (order['sell_amount']/order['buy_amount']) * m_sell_amount,
+                        creator_id=order_id)
+    session.add(order_obj)
+    session.commit()
+
+  elif order['buy_amount'] < m_sell_amount:
+    order_obj = Order(  sender_pk=m_sender_pk,
+                        receiver_pk=m_receiver_pk, 
+                        buy_currency=m_buy_currency, 
+                        sell_currency=m_sell_currency, 
+                        buy_amount= m_buy_amount - (m_buy_amount/m_sell_amount) * order['buy_amount'], 
+                        sell_amount= m_sell_amount - order['buy_amount'],
+                        creator_id=m_order_id)
+    session.add(order_obj)
+    session.commit()
